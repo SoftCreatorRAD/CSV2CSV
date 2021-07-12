b3481ce00bc97c3e9a19d0019489cef8852abb37
Vasiliy <VGoncharenko@QCRing.com>
2021-07-12 13:51:18 -0400

    Implemented the required functionality

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace csv2csv
 {
@@ -10,6 +9,70 @@ namespace csv2csv
 	{
 		static void Main(string[] args)
 		{
+			if (args.Length < 1)
+			{
+				Console.WriteLine("Insufficient numebr of arguments.");
+				Console.WriteLine("Expected at least full path to source CSV.");
+				Console.WriteLine("Ex.: csv2csv.exe  c:\\temp\\temp.csv");
+				Console.WriteLine("Or full path to source CSV and target CSV");
+				Console.WriteLine("Ex.: csv2csv.exe  c:\\temp\\temp.csv c:\\temp\\temp2.csv");
+				return;
+			}
+
+			var csvSrc = args[0];
+			var csvTrg = (args.Length >= 2) ? args[1] : string.Empty;
+			var trgRow = new StringBuilder();
+			var trgRows = new StringBuilder();
+
+			if (!File.Exists(csvSrc))
+			{
+				Console.WriteLine("Source CSV does not exist.");
+				Console.WriteLine(csvSrc);
+				return;
+			}
+
+			var srcFileName = Path.GetFileNameWithoutExtension(csvSrc);
+			var trgFileName = Path.GetFileNameWithoutExtension(csvTrg);
+			if (csvTrg == string.Empty)
+			{
+				trgFileName = srcFileName + "_result";
+				csvTrg = Path.Combine(Path.GetDirectoryName(csvSrc), trgFileName + ".csv");
+			}
+
+
+			Console.WriteLine("Source: " + csvSrc);
+			Console.WriteLine("Target: " + csvTrg);
+
+			//Console.WriteLine(srcFileName);
+			//Console.WriteLine(trgFileName);
+
+			using (TextFieldParser parser = new TextFieldParser(csvSrc))
+			{
+				int recCounter = 0;
+				parser.TextFieldType = FieldType.Delimited;
+				parser.SetDelimiters(",");
+				while (!parser.EndOfData)
+				{
+					//Processing row
+					trgRow.Clear();
+					string[] fields = parser.ReadFields();
+					foreach (string field in fields)
+					{
+						trgRow.Append(((trgRow.Length > 0) ? "," : "") + "\"" + field.Replace("\"", "") + "\"");
+					}
+					trgRows.AppendLine(trgRow.ToString());
+
+					recCounter++;
+					if (recCounter > 100000) // Buffer size
+					{
+						File.AppendAllText(csvTrg, trgRows.ToString());
+						trgRows.Clear();
+						recCounter = 0;
+					}
+				}
+			}
+			File.AppendAllText(csvTrg, trgRows.ToString());
 		}
+
 	}
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("csv2csv")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Transforms CSV to CSV with field, wrapped in double-quotes.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("csv2csv")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © SfotCreator, LLC., 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
